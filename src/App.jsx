5018a21375bb00f3204281f1b008281563b772ed
rabeya121 <rabeya.hridi04@gmail.com>
2023-10-31 22:36:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Service from './components/Service';
+import Project from './components/Project';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div>
+    <h1>this is one packge</h1>
+
+      <Header />
+      <Hero />
+      <About />
+      <Service />
+      <Project />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+
+
+const Hero = () => {
+    return (
+        <div>
+            <h2>my hero page</h2>
+           <p>this is a big page</p> 
+        </div>
+    );
+};
+
+export default Hero;
